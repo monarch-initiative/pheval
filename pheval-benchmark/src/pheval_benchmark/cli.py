5a485d33dd2b552deaec151d5d505d345ef1b360
Yasemin Bridges <93208398+yaseminbridges@users.noreply.github.com>
2022-11-01 18:08:53 +0000

    Update cli.py

--- pheval-benchmark/src/pheval_benchmark/cli.py
+++ pheval-benchmark/src/pheval_benchmark/cli.py
@@ -2,6 +2,9 @@ import click
 from pheval_benchmark.create_batch_commands import prepare_exomiser_batch
 from pheval_benchmark.assess_prioritisation import assess_prioritisation
 from pheval_benchmark.create_spiked_vcf import spike_vcf
+from pheval_benchmark.create_noisy_phenopackets import create_noisy_phenopackets
+import warnings
+warnings.filterwarnings("ignore")
 
 
 @click.group()
@@ -12,6 +15,8 @@ def pheval_benchmark():
 pheval_benchmark.add_command(prepare_exomiser_batch)
 pheval_benchmark.add_command(assess_prioritisation)
 pheval_benchmark.add_command(spike_vcf)
+pheval_benchmark.add_command(create_noisy_phenopackets)
+
 
 if __name__ == '__main__':
     pheval_benchmark()
