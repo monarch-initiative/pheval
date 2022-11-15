76e28deb628a46a99df49517085400238f2cc3a6
Yasemin Bridges <93208398+yaseminbridges@users.noreply.github.com>
2022-11-15 11:52:40 +0000

    Add files via upload

---
+++ pheval-benchmark/src/pheval_benchmark/utils/phenopacket_utils.py
@@ -0,0 +1,71 @@
+import json
+import os
+from collections import defaultdict
+from phenopackets import Phenopacket, Family
+from google.protobuf.json_format import Parse
+from pheval_benchmark.custom_exceptions import IncompatibleGenomeAssemblyError, IncorrectFileFormatError
+
+
+class PhenopacketReader:
+    """ Class for reading Phenopackets and retrieving relevant data. """
+
+    def __init__(self, file: str):
+        self.file_name = file
+        file = open(file, "r")
+        phenopacket = json.load(file)
+        if "proband" in phenopacket:
+            self.pheno = Parse(json.dumps(phenopacket), Family())
+        else:
+            self.pheno = Parse(json.dumps(phenopacket), Phenopacket())
+        file.close()
+
+    def interpretations(self) -> list:
+        if hasattr(self.pheno, 'proband'):
+            pheno_interpretation = self.pheno.proband.interpretations
+        else:
+            pheno_interpretation = self.pheno.interpretations
+        return pheno_interpretation
+
+    def files(self) -> list:
+        return self.pheno.files
+
+    def vcf_file_data(self, vcf_dir):
+        compatible_genome_assembly = ["GRCh37", "hg19", "GRCh38", "hg38"]
+        ppacket_files = self.files()
+        vcf, genome_assembly = "", ""
+        for file in ppacket_files:
+            vcf_name = ""
+            if file.file_attributes["fileFormat"].upper() == "VCF":
+                vcf_path = file.uri
+                if "/" in vcf_path:
+                    vcf_name = vcf_path.rsplit('/')[-1]
+                if "/" not in vcf_path:
+                    vcf_name = vcf_path
+            if not vcf_name.endswith(".vcf") and not vcf_name.endswith(".vcf.gz"):
+                raise IncorrectFileFormatError(vcf_name, ".vcf or .vcf.gz file")
+            vcf = os.path.join(vcf_dir, vcf_name)
+            assembly = file.file_attributes["genomeAssembly"]
+            if assembly not in compatible_genome_assembly:
+                raise IncompatibleGenomeAssemblyError(assembly, self.file_name)
+            genome_assembly = assembly
+        return vcf, genome_assembly
+
+    @staticmethod
+    def diagnosed_genes(pheno_interpretation: list) -> list:
+        genes = []
+        for i in pheno_interpretation:
+            for g in i.diagnosis.genomic_interpretations:
+                genes.append(g.variant_interpretation.variation_descriptor.gene_context.symbol)
+        genes = list(set(genes))
+        return genes
+
+    @staticmethod
+    def diagnosed_variants(pheno_interpretation: list) -> list:
+        variants = []
+        for i in pheno_interpretation:
+            for g in i.diagnosis.genomic_interpretations:
+                variant = defaultdict(dict)
+                variant["geneSymbol"] = g.variant_interpretation.variation_descriptor.gene_context.symbol
+                variant["variant"] = g.variant_interpretation.variation_descriptor.vcf_record
+                variants.append(variant)
+        return variants
