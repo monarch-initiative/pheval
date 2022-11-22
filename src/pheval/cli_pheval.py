768fe08a349cf82e6ac302ab0cb61370c105c25a
Vinicius de Souza <souzadevinicius@gmail.com>
2022-11-22 07:06:45 -0300

    flake8 line break

--- src/pheval/cli_pheval.py
+++ src/pheval/cli_pheval.py
@@ -9,12 +9,48 @@ from .runners.exomiser_pheval_runner import ExomiserPhEvalRunner
 
 
 @click.command()
-@click.option("--inputdir", "-i", metavar="INPUTDIR", required=True, help="The input directory (relative path: e.g exomiser-13.11)")
-@click.option( "--testdatadir", "-t", metavar="TESTDATA", required=True, help="The input directory (relative path: e.g ./data)" )
-@click.option("--runner", "-r", metavar="RUNNER", required=True, help="Runner implementation (e.g exomiser-13.11)")
-@click.option("--tmpdir", "-m", metavar="TMPDIR", required=False, help="The path of the temporary directory (optional)")
-@click.option("--outputdir", "-o", metavar="OUTPUTDIR", required=True, help="The path of the output directory")
-@click.option("--config", "-c", metavar="CONFIG", required=False, help="The path of the configuration file (optional e.g config.yaml)")
+@click.option(
+    "--inputdir",
+    "-i",
+    metavar="INPUTDIR",
+    required=True,
+    help="The input directory (relative path: e.g exomiser-13.11)",
+)
+@click.option(
+    "--testdatadir",
+    "-t",
+    metavar="TESTDATA",
+    required=True,
+    help="The input directory (relative path: e.g ./data)",
+)
+@click.option(
+    "--runner",
+    "-r",
+    metavar="RUNNER",
+    required=True,
+    help="Runner implementation (e.g exomiser-13.11)",
+)
+@click.option(
+    "--tmpdir",
+    "-m",
+    metavar="TMPDIR",
+    required=False,
+    help="The path of the temporary directory (optional)",
+)
+@click.option(
+    "--outputdir",
+    "-o",
+    metavar="OUTPUTDIR",
+    required=True,
+    help="The path of the output directory",
+)
+@click.option(
+    "--config",
+    "-c",
+    metavar="CONFIG",
+    required=False,
+    help="The path of the configuration file (optional e.g config.yaml)",
+)
 def run(inputdir, testdatadir, runner, tmpdir, outputdir, config) -> None:
     """PhEval Runner Command Line Interface
 
