39373506050dfa9f3dc8cf6b2c78f6a238ecdfc1
Yasemin Bridges <y.bridges@qmul.ac.uk>
2022-12-08 20:37:09 +0000

    #34 adding tests for spiked vcf code

--- tests/test_create_spiked_vcf.py
+++ tests/test_create_spiked_vcf.py
@@ -2,7 +2,8 @@ import os
 import unittest
 from pathlib import Path
 
-from pheval.prepare import create_spiked_vcf
+from pheval.prepare.create_spiked_vcf import VcfPicker, VcfParser, VcfHeader, ProbandVariantChecker, VcfSpiker
+from pheval.utils.phenopacket_utils import CausativeVariant
 
 
 class TestVcfPicker(unittest.TestCase):
@@ -11,40 +12,47 @@ class TestVcfPicker(unittest.TestCase):
         cls.vcf_file = Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_1.vcf")
         cls.vcf_dir = Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir")
 
-    def test_vcf_picker(self):
-        self.assertEqual(
-            "test_1.vcf",
-            create_spiked_vcf.VcfPicker(self.vcf_file, None).pick_file().name,
-        )
-        selected_file = create_spiked_vcf.VcfPicker(None, self.vcf_dir).pick_file()
-        self.assertTrue("test_1.vcf" == selected_file.name or "test_2.vcf" == selected_file.name)
+    def test_pick_file_from_dir(self):
+        self.assertTrue(
+            "test_1.vcf" or "test_2.vcf" == VcfPicker(None, self.vcf_dir).pick_file_from_dir())
+
+    def test_pick_file(self):
+        self.assertEqual("test_1.vcf", VcfPicker(self.vcf_file, None).pick_file().name)
 
 
 class TestVcfParser(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.hg19_file = create_spiked_vcf.VcfParser(
-            Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf")
-        )
-        cls.hg38_file = create_spiked_vcf.VcfParser(
-            Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_2.vcf")
-        )
+        cls.hg19_file = VcfParser(
+                    Path(
+                        os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf"
+                    ),
+                    False,
+                )
+        cls.hg38_file = VcfParser(
+                    Path(
+                        os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_2.vcf"
+                    ),
+                    False,
+                )
+
+    def test_parse_assembly(self):
+        self.assertEqual(self.hg19_file.parse_assembly(), ("GRCh37", True))
+        self.assertEqual(self.hg38_file.parse_assembly(), ("GRCh38", False))
+
+    def test_parse_sample_id(self):
+        self.assertEqual(self.hg19_file.parse_sample_id(), "TEMPLATE")
+        self.assertEqual(self.hg38_file.parse_sample_id(), "HG001")
 
     def test_parse_header(self):
-        self.assertEqual(
-            create_spiked_vcf.VcfHeader("TEMPLATE", "GRCh37", True),
-            self.hg19_file.parse_header(),
-        )
-        self.assertEqual(
-            create_spiked_vcf.VcfHeader("HG001", "GRCh38", True), self.hg38_file.parse_header()
-        )
+        self.assertTrue(type(self.hg19_file.parse_header()) == VcfHeader)
 
 
 class TestProbandVariantChecker(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.causative_variants = [
-            create_spiked_vcf.CausativeVariant(
+            CausativeVariant(
                 Path(
                     os.path.dirname(os.path.realpath(__file__))
                     + "/input_dir/test_phenopacket_1.json"
@@ -58,18 +66,18 @@ class TestProbandVariantChecker(unittest.TestCase):
                 "heterozygous",
             )
         ]
-        cls.vcf_header_compatible = create_spiked_vcf.VcfHeader("TEMPLATE", "GRCh37", True)
-        cls.vcf_header_incompatible = create_spiked_vcf.VcfHeader("HG001", "GRCh38", True)
+        cls.vcf_header_compatible = VcfHeader("TEMPLATE", "GRCh37", True)
+        cls.vcf_header_incompatible = VcfHeader("HG001", "GRCh38", False)
 
     def test_check_variant_assembly(self):
         self.assertTrue(
-            create_spiked_vcf.ProbandVariantChecker(
+            ProbandVariantChecker(
                 self.causative_variants, self.vcf_header_compatible
             ).check_variant_assembly()
             == []
         )
         self.assertTrue(
-            create_spiked_vcf.ProbandVariantChecker(
+            ProbandVariantChecker(
                 self.causative_variants, self.vcf_header_incompatible
             ).check_variant_assembly()
             != []
@@ -79,7 +87,7 @@ class TestProbandVariantChecker(unittest.TestCase):
 class TestVcfSpiker(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.vcf_spiker = create_spiked_vcf.VcfSpiker(
+        cls.vcf_spiker = VcfSpiker(
             Path(
                 os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_phenopacket_1.json"
             ),
@@ -88,7 +96,7 @@ class TestVcfSpiker(unittest.TestCase):
             ),
             Path("test_output_dir/"),
             [
-                create_spiked_vcf.CausativeVariant(
+                CausativeVariant(
                     Path(
                         os.path.dirname(os.path.realpath(__file__))
                         + "/input_dir/test_phenopacket_1.json"
@@ -102,9 +110,9 @@ class TestVcfSpiker(unittest.TestCase):
                     "heterozygous",
                 )
             ],
-            create_spiked_vcf.VcfHeader("TEMPLATE", "GRCh37", True),
+            VcfHeader("TEMPLATE", "GRCh37", True),
         )
-        cls.variant = create_spiked_vcf.CausativeVariant(
+        cls.variant = CausativeVariant(
             Path(
                 os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_phenopacket_1.json"
             ),
@@ -126,7 +134,7 @@ class TestVcfSpiker(unittest.TestCase):
         )
 
     def test_construct_vcf_records(self):
-        vcf_contents = self.vcf_spiker.construct_vcf_records()
+        vcf_contents = self.vcf_spiker.construct_vcf_records(False)
         self.assertTrue(len(vcf_contents) == 68)
         self.assertTrue(
             "chr1" in vcf_contents[59],
@@ -134,63 +142,8 @@ class TestVcfSpiker(unittest.TestCase):
         )
 
     def test_construct_header(self):
-        updated_vcf_contents = self.vcf_spiker.construct_header()
+        updated_vcf_contents = self.vcf_spiker.construct_header(False)
         for line in updated_vcf_contents:
             if line.startswith("#CHROM"):
                 self.assertTrue("TEST1" in line and "TEMPLATE" not in line)
-
-
-class TestVcfWriter(unittest.TestCase):
-    vcf_contents = create_spiked_vcf.VcfSpiker(
-        Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_phenopacket_1.json"),
-        Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf"),
-        Path("test_output_dir/"),
-        [
-            create_spiked_vcf.CausativeVariant(
-                Path(
-                    os.path.dirname(os.path.realpath(__file__))
-                    + "/input_dir/test_phenopacket_1.json"
-                ),
-                "TEST1",
-                "GRCh37",
-                "1",
-                886190,
-                "G",
-                "A",
-                "heterozygous",
-            )
-        ],
-        create_spiked_vcf.VcfHeader("TEMPLATE", "GRCh37", True),
-    ).construct_header()
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.vcf_writer = create_spiked_vcf.VcfWriter(
-            Path(
-                os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_phenopacket_1.json"
-            ),
-            cls.vcf_contents,
-            Path(Path(os.path.dirname(os.path.realpath(__file__)) + "/test_copied_vcf.json")),
-        )
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        Path.unlink(
-            Path(Path(os.path.dirname(os.path.realpath(__file__)) + "/test_copied_vcf.json"))
-        )
-
-    def test_copy_template_to_new_file(self):
-        self.vcf_writer.copy_template_to_new_file()
-        self.assertTrue(
-            os.path.exists(
-                Path(Path(os.path.dirname(os.path.realpath(__file__)) + "/test_copied_vcf.json"))
-            )
-        )
-
-    def test_write_vcf(self):
-        self.vcf_writer.write_vcf()
-        with open(
-            Path(Path(os.path.dirname(os.path.realpath(__file__)) + "/test_copied_vcf.json"))
-        ) as f:
-            self.assertEqual(len(f.readlines()), 68)
-        f.close()
+#
