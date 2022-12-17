390728824dbbf8c15813b9d6443162f17ca522c7
Yasemin Bridges <y.bridges@qmul.ac.uk>
2022-12-17 22:15:51 +0000

    fixing tests

--- tests/test_create_spiked_vcf.py
+++ tests/test_create_spiked_vcf.py
@@ -2,46 +2,70 @@ import os
 import unittest
 from pathlib import Path
 
-from pheval.prepare.create_spiked_vcf import (
-    ProbandVariantChecker,
+from pheval.prepare.create_spiked_vcf import (  # VcfParser,; VcfSpiker,
     VcfHeader,
-    VcfParser,
+    VcfHeaderParser,
     VcfPicker,
     VcfSpiker,
+    check_variant_assembly,
+    read_vcf,
 )
-from pheval.utils.phenopacket_utils import ProbandCausativeVariant, VariantData
+from pheval.utils.phenopacket_utils import (
+    IncompatibleGenomeAssemblyError,
+    ProbandCausativeVariant,
+    VariantData,
+)
+
+hg19_vcf = read_vcf(
+    Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf")
+)
+hg38_vcf = read_vcf(
+    Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_2.vcf.gz")
+)
 
 
 class TestVcfPicker(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.vcf_file = Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_1.vcf")
-        cls.vcf_dir = Path(os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir")
+        cls.vcf_file = Path(
+            os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf"
+        )
+        cls.vcf_dir = Path(
+            os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_2.vcf.gz"
+        )
 
     def test_pick_file_from_dir(self):
         self.assertTrue(
-            "test_1.vcf" or "test_2.vcf" == VcfPicker(None, self.vcf_dir).pick_file_from_dir()
+            "test_1.vcf" or "test_2.vcf.gz" == VcfPicker(None, self.vcf_dir).pick_file_from_dir()
         )
 
     def test_pick_file(self):
         self.assertEqual("test_1.vcf", VcfPicker(self.vcf_file, None).pick_file().name)
 
 
-class TestVcfParser(unittest.TestCase):
+class TestReadVcf(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.hg19_file = VcfParser(
-            Path(
-                os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf"
-            ),
-            False,
+        cls.vcf_file = Path(
+            os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf"
         )
-        cls.hg38_file = VcfParser(
-            Path(
-                os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_2.vcf"
-            ),
-            False,
+        cls.gzipped_vcf_file = Path(
+            os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_2.vcf.gz"
         )
+
+    def test_read_vcf(self):
+        self.assertTrue(type(read_vcf(self.vcf_file)), list[str])
+
+    def test_read_vcf_gzipped(self):
+        for line in read_vcf(self.gzipped_vcf_file):
+            self.assertFalse(isinstance(line, (bytes, bytearray)))
+
+
+class TestVcfHeaderParser(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.hg19_file = VcfHeaderParser(hg19_vcf)
+        cls.hg38_file = VcfHeaderParser(hg38_vcf)
 
     def test_parse_assembly(self):
         self.assertEqual(self.hg19_file.parse_assembly(), ("GRCh37", True))
@@ -51,11 +75,12 @@ class TestVcfParser(unittest.TestCase):
         self.assertEqual(self.hg19_file.parse_sample_id(), "TEMPLATE")
         self.assertEqual(self.hg38_file.parse_sample_id(), "HG001")
 
-    def test_parse_header(self):
-        self.assertTrue(type(self.hg19_file.parse_vcf_header()) == VcfHeader)
+    def test_parse_vcf_header(self):
+        self.assertEqual(self.hg19_file.parse_vcf_header(), VcfHeader("TEMPLATE", "GRCh37", True))
+        self.assertEqual(self.hg38_file.parse_vcf_header(), VcfHeader("HG001", "GRCh38", False))
 
 
-class TestProbandVariantChecker(unittest.TestCase):
+class TestCheckVariantAssembly(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.causative_variants = [
@@ -66,31 +91,45 @@ class TestProbandVariantChecker(unittest.TestCase):
                 "heterozygous",
             )
         ]
+        cls.incorrect_causative_variant_assembly = [
+            ProbandCausativeVariant(
+                "TEST1",
+                "hg10",
+                VariantData("1", 886190, "G", "A"),
+                "heterozygous",
+            )
+        ]
         cls.vcf_header_compatible = VcfHeader("TEMPLATE", "GRCh37", True)
         cls.vcf_header_incompatible = VcfHeader("HG001", "GRCh38", False)
 
     def test_check_variant_assembly(self):
-        self.assertTrue(
-            ProbandVariantChecker(
+        self.assertEqual(
+            check_variant_assembly(
                 self.causative_variants, self.vcf_header_compatible, Path("/path/to/phenopacket")
-            ).check_variant_assembly()
-            == []
+            ),
+            None,
         )
-        self.assertTrue(
-            ProbandVariantChecker(
+
+    def test_check_variant_assembly_incompatible_vcf(self):
+        with self.assertRaises(IncompatibleGenomeAssemblyError):
+            check_variant_assembly(
                 self.causative_variants, self.vcf_header_incompatible, Path("/path/to/phenopacket")
-            ).check_variant_assembly()
-            != []
-        )
+            )
+
+    def test_check_variant_assembly_incorrect_variant_assembly(self):
+        with self.assertRaises(IncompatibleGenomeAssemblyError):
+            check_variant_assembly(
+                self.incorrect_causative_variant_assembly,
+                self.vcf_header_incompatible,
+                Path("/path/to/phenopacket"),
+            )
 
 
 class TestVcfSpiker(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.vcf_spiker = VcfSpiker(
-            Path(
-                os.path.dirname(os.path.realpath(__file__)) + "/input_dir/test_vcf_dir/test_1.vcf"
-            ),
+            hg19_vcf,
             [
                 ProbandCausativeVariant(
                     "TEST1",
@@ -98,6 +137,24 @@ class TestVcfSpiker(unittest.TestCase):
                     VariantData("1", 886190, "G", "A"),
                     "heterozygous",
                 )
+            ],
+            VcfHeader("TEMPLATE", "GRCh37", True),
+        )
+        cls.vcf_spiker_multiple_variants = VcfSpiker(
+            hg19_vcf,
+            [
+                ProbandCausativeVariant(
+                    "TEST1",
+                    "GRCh37",
+                    VariantData("1", 886190, "G", "A"),
+                    "heterozygous",
+                ),
+                ProbandCausativeVariant(
+                    "TEST1",
+                    "GRCh37",
+                    VariantData("3", 61580860, "G", "A"),
+                    "homozygous",
+                ),
             ],
             VcfHeader("TEMPLATE", "GRCh37", True),
         )
@@ -109,23 +166,48 @@ class TestVcfSpiker(unittest.TestCase):
         )
 
     def test_construct_variant(self):
-        variant_record = self.vcf_spiker.construct_variant(self.variant)
-        self.assertEqual(type(variant_record), list)
-        self.assertEqual(len(variant_record), 10)
-        self.assertTrue(
-            "chr1" in variant_record and "886190" in variant_record and "0/1" in variant_record[9]
+        self.assertEqual(
+            self.vcf_spiker.construct_variant_entry(self.variant),
+            [
+                "chr1",
+                "886190",
+                ".",
+                "G",
+                "A",
+                "100",
+                "PASS",
+                "SPIKED_VARIANT_HETEROZYGOUS",
+                "GT:AD:DP:GQ:PL",
+                "0/1:0,2:2:12:180,12,0\n",
+            ],
         )
 
-    def test_construct_vcf_records(self):
-        vcf_contents = self.vcf_spiker.construct_vcf_records(False)
-        self.assertTrue(len(vcf_contents) == 68)
-        self.assertTrue(
-            "chr1" in vcf_contents[59],
-            "886190" in vcf_contents[59] and "SPIKED_VARIANT" in vcf_contents[59],
+    def test_construct_vcf_records_single_variant(self):
+        self.assertEqual(
+            self.vcf_spiker.construct_vcf_records()[59],
+            "chr1\t886190\t.\tG\tA\t100\tPASS\tSPIKED_VARIANT_HETEROZYGOUS\t"
+            "GT:AD:DP:GQ:PL\t0/1:0,2:2:12:180,12,0\n",
+        )
+
+    def test_construct_vcf_records_multiple_variants(self):
+        updated_records = self.vcf_spiker_multiple_variants.construct_vcf_records()
+        self.assertEqual(
+            updated_records[59],
+            "chr1\t886190\t.\tG\tA\t100\tPASS\tSPIKED_VARIANT_HETEROZYGOUS\t"
+            "GT:AD:DP:GQ:PL\t0/1:0,2:2:12:180,12,0\n",
+        )
+        self.assertEqual(
+            updated_records[64],
+            "chr3\t61580860\t.\tG\tA\t100\tPASS\tSPIKED_VARIANT_HOMOZYGOUS\t"
+            "GT:AD:DP:GQ:PL\t1/1:0,2:2:12:180,12,0\n",
         )
 
     def test_construct_header(self):
-        updated_vcf_contents = self.vcf_spiker.construct_header(False)
-        for line in updated_vcf_contents:
-            if line.startswith("#CHROM"):
-                self.assertTrue("TEST1" in line and "TEMPLATE" not in line)
+        self.assertEqual(
+            [
+                line
+                for line in self.vcf_spiker.construct_header(hg19_vcf)
+                if line.startswith("#CHROM")
+            ],
+            ["#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tTEST1\n"],
+        )
