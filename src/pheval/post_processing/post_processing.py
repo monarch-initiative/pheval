81a6ffceb0e9f1c4ff31a546d1d2b218b70094f2
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-02-06 11:10:48 +0000

    tox reformatting

--- src/pheval/post_processing/post_processing.py
+++ src/pheval/post_processing/post_processing.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class PhEvalGeneResult:
     """Minimal data required from tool-specific output for gene prioritisation."""
+
     gene_symbol: str
     gene_identifier: str
     score: float
@@ -12,20 +13,24 @@ class PhEvalGeneResult:
 @dataclass
 class RankedPhEvalGeneResult:
     """PhEval gene result with corresponding rank."""
+
     pheval_gene_result: PhEvalGeneResult
     rank: int
 
     def as_dict(self):
         """Return PhEval gene result as dictionary."""
-        return {'gene_symbol': self.pheval_gene_result.gene_symbol,
-                'gene_identifier': self.pheval_gene_result.gene_identifier,
-                'score': self.pheval_gene_result.score,
-                'rank': self.rank}
+        return {
+            "gene_symbol": self.pheval_gene_result.gene_symbol,
+            "gene_identifier": self.pheval_gene_result.gene_identifier,
+            "score": self.pheval_gene_result.score,
+            "rank": self.rank,
+        }
 
 
 @dataclass
 class PhEvalVariantResult:
     """Minimal data required from tool-specific output for variant prioritisation."""
+
     chromosome: str
     start: int
     end: int
@@ -37,15 +42,18 @@ class PhEvalVariantResult:
 @dataclass
 class RankedPhEvalVariantResult:
     """PhEval variant result with corresponding rank."""
+
     pheval_variant_result: PhEvalVariantResult
     rank: int
 
     def as_dict(self):
         """Return PhEval variant result as dictionary."""
-        return {'chromosome': self.pheval_variant_result.chromosome,
-                'start': self.pheval_variant_result.start,
-                'end': self.pheval_variant_result.end,
-                'ref': self.pheval_variant_result.ref,
-                'alt': self.pheval_variant_result.alt,
-                'score': self.pheval_variant_result.score,
-                'rank': self.rank}
+        return {
+            "chromosome": self.pheval_variant_result.chromosome,
+            "start": self.pheval_variant_result.start,
+            "end": self.pheval_variant_result.end,
+            "ref": self.pheval_variant_result.ref,
+            "alt": self.pheval_variant_result.alt,
+            "score": self.pheval_variant_result.score,
+            "rank": self.rank,
+        }
