f4babd3a1cb4c5aeadf03a521ad7139faf623176
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-02-06 13:00:32 +0000

    adding dataclass for adding ranks to a result

--- src/pheval/post_processing/post_processing.py
+++ src/pheval/post_processing/post_processing.py
@@ -79,3 +79,24 @@ class ResultSorter:
             self.sort_by_increasing_score() if self.ranking_method.lower() == "pvalue"
             else self.sort_by_decreasing_score()
         )
+
+
+@dataclass
+class ScoreRanker:
+    rank: int = 0
+    current_score: float = float('inf')
+    count: int = 0
+
+    def rank_scores(self, round_score):
+        """Add ranks to a result, equal scores are given the same rank e.g., 1,1,3."""
+        if round_score > self.current_score:
+            raise ValueError(
+                f"Input score {round_score} is greater than previous score of {self.current_score}. "
+                f"Scores must be provided in reverse numerical order i.e. highest to lowest.")
+        self.count += 1
+        if self.current_score == round_score:
+            return self.rank
+        self.current_score = round_score
+        self.rank = self.count
+        return self.rank
+
