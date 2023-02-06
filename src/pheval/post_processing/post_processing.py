889eb89be8a6dc95a0edb9ce5bdcc93d05fc9d9e
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-02-06 13:44:57 +0000

    removing thrown exception as it won't work for pvalue cases

--- src/pheval/post_processing/post_processing.py
+++ src/pheval/post_processing/post_processing.py
@@ -92,11 +92,6 @@ class ScoreRanker:
 
     def rank_scores(self, round_score):
         """Add ranks to a result, equal scores are given the same rank e.g., 1,1,3."""
-        if round_score > self.current_score:
-            raise ValueError(
-                f"Input score {round_score} is greater than previous score of {self.current_score}. "
-                f"Scores must be provided in reverse numerical order i.e. highest to lowest."
-            )
         self.count += 1
         if self.current_score == round_score:
             return self.rank
