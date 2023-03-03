021b8c206c1206a008e5014d4a6d185455eca9d4
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-03 16:54:42 +0000

    adding merged command for spiking vcfs

--- src/pheval/cli_pheval_utils.py
+++ src/pheval/cli_pheval_utils.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import click
 
 from pheval.prepare.create_noisy_phenopackets import scramble_phenopackets
+from pheval.prepare.create_spiked_vcf import spike_vcfs
 from pheval.prepare.custom_exceptions import InputError, MutuallyExclusiveOptionError
 from pheval.prepare.update_phenopacket import update_phenopackets
 from pheval.utils.semsim_utils import percentage_diff, semsim_heatmap_plot
@@ -183,3 +184,63 @@ def update_phenopackets_command(
     if phenopacket_path is None and phenopacket_dir is None:
         raise InputError("Either a phenopacket or phenopacket directory must be specified")
     update_phenopackets(gene_identifier, phenopacket_path, phenopacket_dir, output_dir)
+
+
+@click.command("create-spiked-vcfs")
+@click.option(
+    "--phenopacket-path",
+    "-p",
+    metavar="PATH",
+    help="Path to phenopacket.",
+    type=Path,
+    cls=MutuallyExclusiveOptionError,
+    mutually_exclusive=["phenopacket_dir"],
+)
+@click.option(
+    "--phenopacket-dir",
+    "-P",
+    metavar="PATH",
+    help="Path to phenopacket directory for updating.",
+    type=Path,
+    cls=MutuallyExclusiveOptionError,
+    mutually_exclusive=["phenopacket_path"],
+)
+@click.option(
+    "--template-vcf-path",
+    "-t",
+    cls=MutuallyExclusiveOptionError,
+    metavar="PATH",
+    required=False,
+    help="Template VCF file",
+    mutually_exclusive=["vcf_dir"],
+    type=Path,
+)
+@click.option(
+    "--vcf-dir",
+    "-v",
+    cls=MutuallyExclusiveOptionError,
+    metavar="PATH",
+    help="Directory containing template VCF files",
+    mutually_exclusive=["template_vcf"],
+    type=Path,
+)
+@click.option(
+    "--output-dir",
+    "-O",
+    metavar="PATH",
+    required=True,
+    help="Path for creation of output directory",
+    default="vcf",
+    type=Path,
+)
+def create_spiked_vcfs_command(
+    phenopacket_path: Path,
+    phenopacket_dir: Path,
+    output_dir: Path,
+    template_vcf_path: Path = None,
+    vcf_dir: Path = None,
+):
+    """Spikes variants into a template VCF file for a directory of phenopackets."""
+    if phenopacket_path is None and phenopacket_dir is None:
+        raise InputError("Either a phenopacket or phenopacket directory must be specified")
+    spike_vcfs(output_dir, phenopacket_path, phenopacket_dir, template_vcf_path, vcf_dir)
