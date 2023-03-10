b55e59d28f2b2f30dce437da3212a7a5b4a8cd27
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-10 16:30:51 +0000

    adding constants for tool results dir and runner input commands dir

--- src/pheval/utils/constants.py
+++ src/pheval/utils/constants.py
@@ -1,2 +1,4 @@
 PHEVAL_GENE_RESULTS_DIR = "pheval_gene_results/"
 PHEVAL_VARIANT_RESULTS_DIR = "pheval_variant_results/"
+TOOL_RESULTS_DIR = "results/"
+RUNNER_INPUT_COMMANDS_DIR = "runner_input_commands/"
