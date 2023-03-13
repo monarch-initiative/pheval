a9401f4c7cb507e35e15637bb871fdbfceba9b3c
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-13 13:33:59 +0000

    adding tests for the naming of output subdirectories

---
+++ tests/test_runner.py
@@ -0,0 +1,58 @@
+import unittest
+from pathlib import Path
+
+from pheval.runners.runner import DefaultPhEvalRunner
+
+
+class TestDefaultPhEvalRunner(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.pheval_runner = DefaultPhEvalRunner(
+            input_dir="./tests/input_dir/configs/default/input_dir_config.yaml",
+            testdata_dir="/corpora/corpus1/default",
+            output_dir="/results",
+            version="1.0.0",
+            config_file=None,
+            tmp_dir=None,
+        )
+
+    def test__get_tool(self):
+        self.assertEqual(self.pheval_runner._get_tool(), "defaultrunner")
+
+    def test__get_phenotype_only(self):
+        self.assertEqual(self.pheval_runner._get_phenotype_only(), False)
+
+    def test_runner_version_dir(self):
+        self.assertEqual(
+            self.pheval_runner.runner_version_dir, Path("/results/defaultrunner-1.0.0")
+        )
+
+    def test_runner_input_commands_dir(self):
+        self.assertEqual(
+            self.pheval_runner.runner_input_commands_dir,
+            Path("/results/defaultrunner-1.0.0/runner_input_commands"),
+        )
+
+    def test_corpus_variant_dir(self):
+        self.assertEqual(
+            self.pheval_runner.corpus_variant_dir,
+            Path("/results/defaultrunner-1.0.0/default-corpus1-default"),
+        )
+
+    def test_runner_results_dir(self):
+        self.assertEqual(
+            self.pheval_runner.runner_results_dir,
+            Path("/results/defaultrunner-1.0.0/default-corpus1-default/results"),
+        )
+
+    def test_pheval_gene_results_dir(self):
+        self.assertEqual(
+            self.pheval_runner.pheval_gene_results_dir,
+            Path("/results/defaultrunner-1.0.0/default-corpus1-default/pheval_gene_results"),
+        )
+
+    def test_pheval_variant_results_dir(self):
+        self.assertEqual(
+            self.pheval_runner.pheval_variant_results_dir,
+            Path("/results/defaultrunner-1.0.0/default-corpus1-default/pheval_variant_results"),
+        )
