64f2f806b2353a32ef329a5bf8c36d7fc92c5e9a
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-13 12:21:23 +0000

    expanding PhEvalRunner with getters for various output subdirectories, implemented a concrete method to create subdirectories before pheval prepare

--- src/pheval/runners/runner.py
+++ src/pheval/runners/runner.py
@@ -3,7 +3,13 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
 
-from pheval.utils.path_getters import PhEvalResultsDirectoryStructure
+from pheval.config_parser import parse_input_dir_config
+from pheval.utils.constants import (
+    PHEVAL_GENE_RESULTS_DIR,
+    PHEVAL_VARIANT_RESULTS_DIR,
+    RUNNER_INPUT_COMMANDS_DIR,
+    TOOL_RESULTS_DIR,
+)
 
 
 @dataclass
@@ -16,15 +22,91 @@ class PhEvalRunner(ABC):
     output_dir: Path
     config_file: Path
     version: str
+    directory_path = None
+
+    def _get_tool(self):
+        return parse_input_dir_config(self.input_dir).tool
+
+    def _get_phenotype_only(self):
+        return parse_input_dir_config(self.input_dir).phenotype_only
+
+    @property
+    def runner_version_dir(self):
+        return Path(self.output_dir).joinpath(f"{self._get_tool()}-{self.version}")
+
+    @runner_version_dir.setter
+    def runner_version_dir(self, directory_path):
+        self.directory_path = Path(directory_path)
+
+    @property
+    def runner_input_commands_dir(self):
+        return Path(self.output_dir).joinpath(
+            f"{self._get_tool()}-{self.version}/{RUNNER_INPUT_COMMANDS_DIR}"
+        )
+
+    @runner_input_commands_dir.setter
+    def runner_input_commands_dir(self, directory_path):
+        self.directory_path = Path(directory_path)
+
+    @property
+    def corpus_variant_dir(self):
+        return Path(self.output_dir).joinpath(
+            f"{self._get_tool()}-{self.version}/{Path(self.input_dir.parent.name)}-"
+            f"{Path(self.testdata_dir).parent.name}-"
+            f"{Path(self.testdata_dir).name}"
+        )
+
+    @corpus_variant_dir.setter
+    def corpus_variant_dir(self, directory_path):
+        self.directory_path = Path(directory_path)
+
+    @property
+    def runner_results_dir(self):
+        return Path(self.output_dir).joinpath(
+            f"{self._get_tool()}-{self.version}/{Path(self.input_dir.parent.name)}-"
+            f"{Path(self.testdata_dir).parent.name}-"
+            f"{Path(self.testdata_dir).name}/{TOOL_RESULTS_DIR}"
+        )
+
+    @runner_results_dir.setter
+    def runner_results_dir(self, directory_path):
+        self.directory_path = Path(directory_path)
+
+    @property
+    def pheval_gene_results_dir(self):
+        return Path(self.output_dir).joinpath(
+            f"{self._get_tool()}-{self.version}/{Path(self.input_dir.parent.name)}-"
+            f"{Path(self.testdata_dir).parent.name}-"
+            f"{Path(self.testdata_dir).name}/"
+            f"{PHEVAL_GENE_RESULTS_DIR}"
+        )
+
+    @pheval_gene_results_dir.setter
+    def pheval_gene_results_dir(self, directory_path):
+        self.directory_path = Path(directory_path)
+
+    @property
+    def pheval_variant_results_dir(self):
+        return Path(self.output_dir).joinpath(
+            f"{self._get_tool()}-{self.version}/{Path(self.input_dir.parent.name)}-"
+            f"{Path(self.testdata_dir).parent.name}-"
+            f"{Path(self.testdata_dir).name}/"
+            f"{PHEVAL_VARIANT_RESULTS_DIR}"
+        )
+
+    @pheval_variant_results_dir.setter
+    def pheval_variant_results_dir(self, directory_path):
+        self.directory_path = Path(directory_path)
 
     def build_output_directory_structure(self):
         """build output directory structure"""
-        PhEvalResultsDirectoryStructure(
-            input_dir=self.input_dir,
-            testdata_dir=self.testdata_dir,
-            output_dir=self.output_dir,
-            version=self.version,
-        ).build_directory_structure()
+        self.runner_version_dir.mkdir(exist_ok=True, parents=True)
+        self.runner_input_commands_dir.mkdir(exist_ok=True, parents=True)
+        self.corpus_variant_dir.mkdir(parents=True, exist_ok=True)
+        self.runner_results_dir.mkdir(parents=True, exist_ok=True)
+        self.pheval_gene_results_dir.mkdir(parents=True, exist_ok=True)
+        if not self._get_phenotype_only():
+            self.pheval_variant_results_dir.mkdir(parents=True, exist_ok=True)
 
     @abstractmethod
     def prepare(self) -> str:
