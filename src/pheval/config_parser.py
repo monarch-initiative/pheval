675a3c7f3714b6014d624dc9ce4a588039153266
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-14 11:11:13 +0000

    adding documentation for inputdirconfig dataclass

--- src/pheval/config_parser.py
+++ src/pheval/config_parser.py
@@ -9,6 +9,18 @@ from serde.yaml import from_yaml
 @serde
 @dataclass
 class InputDirConfig:
+    """
+    Class for defining the fields within the input direcotry config.
+
+    Args:
+        tool (str): Name of the tool implementation (e.g. exomiser/phen2gene)
+        software_directory (Path or None): Path to the software directory
+        software_config (Path or None): Path to the software configuration file
+        input_directory (Path): Path to the tool input data
+        phenotype_only (bool): Whether the tool is run with HPO terms only (True) or with variant data (False)
+
+    """
+
     tool: str
     software_directory: Path or None
     software_config: Path or None
