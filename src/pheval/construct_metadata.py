3faa68d3d8a3f01c30dc8cd42d880a651ba36da1
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-15 16:43:22 +0000

    changing - to /

--- src/pheval/construct_metadata.py
+++ src/pheval/construct_metadata.py
@@ -50,9 +50,9 @@ def create_run_metadata(
         run_metadata=BasicOutputRunMetaData(
             tool=input_dir_config.tool,
             tool_version=tool_version,
-            config=f"{Path(input_dir).parent.name}-{Path(input_dir).name}",
+            config=f"{Path(input_dir).parent.name}/{Path(input_dir).name}",
             run_timestamp=int(timeit.default_timer() - start_time),
-            corpus=f"{Path(corpus).parent.name}-{Path(corpus).name}",
+            corpus=f"{Path(corpus).parent.name}/{Path(corpus).name}",
         ),
         run_metadata_filepath=Path(corpus_variant_path).joinpath(RUN_METADATA_FILE),
     )
