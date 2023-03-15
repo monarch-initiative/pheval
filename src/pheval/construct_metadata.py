c5a955d4f740e6e5a0ce08246fa0b33752f06f73
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-15 15:10:38 +0000

    adding methods to construct the metadata for a run

---
+++ src/pheval/construct_metadata.py
@@ -0,0 +1,58 @@
+import timeit
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+from serde import serde
+
+from pheval.config_parser import InputDirConfig
+from pheval.utils.constants import INPUT_DIR_CONFIG, RUN_METADATA_FILE
+
+
+@serde
+@dataclass
+class BasicOutputRunMetaData:
+    """Class for defining variables for the run metadata.
+    Args:
+        tool (str): Name of the tool implementation
+        tool_version (str): Version of the tool implementation
+        config (Path): Path to the config file located in the input directory
+        run_timestamp (int): Time taken for run to complete
+        corpus (Path): Path to corpus used in pheval run
+        tool_specific_configuration_options (Any): Special field that can be overwritten by tool implementations to
+                                                   contain any extra tool specific configurations used in the run
+    """
+
+    tool: str
+    tool_version: str
+    config: Path
+    run_timestamp: int
+    corpus: Path
+    tool_specific_configuration_options: Any = None
+
+
+@dataclass
+class MetaDataRecord:
+    run_metadata: BasicOutputRunMetaData
+    run_metadata_filepath: Path
+
+
+def create_run_metadata(
+    input_dir_config: InputDirConfig,
+    tool_version: str,
+    input_dir: Path,
+    start_time: int,
+    corpus: Path,
+    corpus_variant_path: Path,
+):
+    """Create the basic metadata for a single PhEval run."""
+    return MetaDataRecord(
+        run_metadata=BasicOutputRunMetaData(
+            tool=input_dir_config.tool,
+            tool_version=tool_version,
+            config=str(Path(input_dir).joinpath(INPUT_DIR_CONFIG)),
+            run_timestamp=int(timeit.default_timer() - start_time),
+            corpus=corpus,
+        ),
+        run_metadata_filepath=Path(corpus_variant_path).joinpath(RUN_METADATA_FILE),
+    )
