7a30df0a2eb6dafb04a2fb22eb6cf96cfae6a1f6
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-11 16:39:47 +0100

    creating a valid file uri

--- src/pheval/prepare/create_spiked_vcf.py
+++ src/pheval/prepare/create_spiked_vcf.py
@@ -297,7 +297,7 @@ def generate_spiked_vcf_file(
     )
     VcfWriter(spiked_vcf, spiked_vcf_path).write_vcf_file()
     return File(
-        uri=str(spiked_vcf_path.absolute()),
+        uri=f"file://{str(spiked_vcf_path)}",
         file_attributes={"fileFormat": "VCF", "genomeAssembly": vcf_assembly},
     )
 
