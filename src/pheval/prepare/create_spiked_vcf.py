cdba13d01000db464de0653c57004c3dccacfc96
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-17 15:03:05 +0100

    changing vcf to match phenopacket schema specs

--- src/pheval/prepare/create_spiked_vcf.py
+++ src/pheval/prepare/create_spiked_vcf.py
@@ -298,7 +298,7 @@ def generate_spiked_vcf_file(
     VcfWriter(spiked_vcf, spiked_vcf_path).write_vcf_file()
     return File(
         uri=spiked_vcf_path.as_uri(),
-        file_attributes={"fileFormat": "VCF", "genomeAssembly": vcf_assembly},
+        file_attributes={"fileFormat": "vcf", "genomeAssembly": vcf_assembly},
     )
 
 
