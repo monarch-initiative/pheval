5f91defcc10f3f75026b31ba38d9a2abe02fbef3
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-02 14:22:24 +0100

    decode encoded characters from string

--- src/pheval/prepare/create_spiked_vcf.py
+++ src/pheval/prepare/create_spiked_vcf.py
@@ -2,6 +2,7 @@
 import gzip
 import logging
 import secrets
+import urllib.parse
 from copy import copy
 from dataclasses import dataclass
 from pathlib import Path
@@ -297,7 +298,7 @@ def generate_spiked_vcf_file(
     )
     VcfWriter(spiked_vcf, spiked_vcf_path).write_vcf_file()
     return File(
-        uri=spiked_vcf_path.as_uri(),
+        uri=urllib.parse.unquote(spiked_vcf_path.as_uri()),
         file_attributes={"fileFormat": "vcf", "genomeAssembly": vcf_assembly},
     )
 
