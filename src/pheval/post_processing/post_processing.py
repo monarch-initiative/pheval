cba9b3d0c410f039dcb05808351ab55f59c1c634
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-25 10:28:34 +0100

    removing as_dict() methods and inheriting from parent classes

--- src/pheval/post_processing/post_processing.py
+++ src/pheval/post_processing/post_processing.py
@@ -17,7 +17,7 @@ class PhEvalResult:
 
 
 @dataclass
-class PhEvalGeneResult:
+class PhEvalGeneResult(PhEvalResult):
     """Minimal data required from tool-specific output for gene prioritisation."""
 
     gene_symbol: str
@@ -26,24 +26,14 @@ class PhEvalGeneResult:
 
 
 @dataclass
-class RankedPhEvalGeneResult:
+class RankedPhEvalGeneResult(PhEvalGeneResult):
     """PhEval gene result with corresponding rank."""
 
-    pheval_gene_result: PhEvalGeneResult
     rank: int
-
-    def as_dict(self):
-        """Return PhEval gene result as dictionary."""
-        return {
-            "gene_symbol": self.pheval_gene_result.gene_symbol,
-            "gene_identifier": self.pheval_gene_result.gene_identifier,
-            "score": self.pheval_gene_result.score,
-            "rank": self.rank,
-        }
 
 
 @dataclass
-class PhEvalVariantResult:
+class PhEvalVariantResult(PhEvalResult):
     """Minimal data required from tool-specific output for variant prioritisation."""
 
     chromosome: str
@@ -55,23 +45,10 @@ class PhEvalVariantResult:
 
 
 @dataclass
-class RankedPhEvalVariantResult:
+class RankedPhEvalVariantResult(PhEvalVariantResult):
     """PhEval variant result with corresponding rank."""
 
-    pheval_variant_result: PhEvalVariantResult
     rank: int
-
-    def as_dict(self):
-        """Return PhEval variant result as dictionary."""
-        return {
-            "chromosome": self.pheval_variant_result.chromosome,
-            "start": self.pheval_variant_result.start,
-            "end": self.pheval_variant_result.end,
-            "ref": self.pheval_variant_result.ref,
-            "alt": self.pheval_variant_result.alt,
-            "score": self.pheval_variant_result.score,
-            "rank": self.rank,
-        }
 
 
 class SortOrder(Enum):
