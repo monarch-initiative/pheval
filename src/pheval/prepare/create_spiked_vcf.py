b031436159a09b0e857e2d19519710498152e0b2
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-25 10:19:26 +0100

    changing default field to '.'

--- src/pheval/prepare/create_spiked_vcf.py
+++ src/pheval/prepare/create_spiked_vcf.py
@@ -205,9 +205,7 @@ class VcfSpiker:
             else proband_variant_data.variant.alt,
             "100",
             "PASS",
-            proband_variant_data.info
-            if proband_variant_data.info
-            else "SPIKED_VARIANT_" + proband_variant_data.genotype.upper(),
+            proband_variant_data.info if proband_variant_data.info else ".",
             "GT",
             genotype_codes[proband_variant_data.genotype.lower()] + "\n",
         ]
