6b01e42cb2cdfc33782341ec29207c548f95ca14
Vinicius de Souza <souzadevinicius@gmail.com>
2023-06-05 22:43:42 +0100

    fixing sql test. adding sql statements to the len count

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,7 +27,7 @@ class TestSemsimUtils(unittest.TestCase):
         with open(output_file, "r") as f:
             lines = f.readlines()
             input = pd.read_csv(semsim_file, sep="\t")
-            assert len(lines) == len(input) + 1
+            assert len(lines) == len(input) + 6
 
     def test_semsim_convert_invalid_format(self):
         semsim_file = "./testdata/semsim/hp-mp.semsim.tsv"
