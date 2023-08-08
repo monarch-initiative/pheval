967f43f604ef33e36386894be2ff7fd88bd66dfd
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-08-08 13:41:59 +0100

    Fixing type hint

--- src/pheval/analyse/parse_pheval_result.py
+++ src/pheval/analyse/parse_pheval_result.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Type
 
 import pandas as pd
 
@@ -11,6 +10,6 @@ def _read_standardised_result(standardised_result_path: Path) -> dict:
     return pd.read_csv(standardised_result_path, delimiter="\t").to_dict("records")
 
 
-def parse_pheval_result(data_class_type: Type, pheval_result: [dict]) -> [PhEvalResult]:
+def parse_pheval_result(data_class_type: PhEvalResult, pheval_result: [dict]) -> [PhEvalResult]:
     """Parse PhEval result into specified dataclass type."""
     return [data_class_type(**row) for row in pheval_result]
