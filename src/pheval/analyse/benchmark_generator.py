2c0bc86d6856e63afdd2da76819eba5c9e886508
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-10-05 14:54:27 +0100

    refactor `BenchmarkPrioritisationOutputGenerator` to `BenchmarkRunOutputGenerator` and adding `benchmark_function` and `rank_comparison_file_suffix` variables to dataclasses

--- src/pheval/analyse/benchmark_generator.py
+++ src/pheval/analyse/benchmark_generator.py
@@ -1,7 +1,13 @@
+from collections import defaultdict
 from dataclasses import dataclass
 from typing import Callable
 
-from pheval.analyse.benchmarking_data import AnalysisResults, TrackRunPrioritisation
+from pheval.analyse.benchmarking_data import BenchmarkRun
+from pheval.analyse.disease_prioritisation_analysis import benchmark_disease_prioritisation
+from pheval.analyse.gene_prioritisation_analysis import benchmark_gene_prioritisation
+from pheval.analyse.rank_stats_writer import RankStatsWriter
+from pheval.analyse.run_data_parser import TrackInputOutputDirectories
+from pheval.analyse.variant_prioritisation_analysis import benchmark_variant_prioritisation
 from pheval.constants import (
     DISEASE_PLOT_FILE_PREFIX,
     DISEASE_PLOT_Y_LABEL,
@@ -13,45 +19,51 @@ from pheval.constants import (
 
 
 @dataclass
-class BenchmarkPrioritisationOutputGenerator:
+class BenchmarkRunOutputGenerator:
     """Base class for recording data required for generating benchmarking outputs."""
 
     prioritisation_type_file_prefix: str
     y_label: str
-    return_function: Callable[[TrackRunPrioritisation], AnalysisResults]
+    benchmark_function: Callable[
+        [TrackInputOutputDirectories, str, float, defaultdict, RankStatsWriter], BenchmarkRun
+    ]
+    rank_comparison_file_suffix: str
 
 
 @dataclass
-class GeneBenchmarkPrioritisationOutputGenerator(BenchmarkPrioritisationOutputGenerator):
-    """Subclass of BenchmarkPrioritisationOutputGenerator specialised
+class GeneBenchmarkRunOutputGenerator(BenchmarkRunOutputGenerator):
+    """Subclass of BenchmarkRunOutputGenerator specialised
     for producing gene prioritisation benchmarking outputs."""
 
     prioritisation_type_file_prefix: str = GENE_PLOT_FILE_PREFIX
     y_label: str = GENE_PLOT_Y_LABEL
-    return_function: Callable[
-        [TrackRunPrioritisation], AnalysisResults
-    ] = TrackRunPrioritisation.return_gene
+    benchmark_function: Callable[
+        [TrackInputOutputDirectories, str, float, defaultdict, RankStatsWriter], BenchmarkRun
+    ] = benchmark_gene_prioritisation
+    rank_comparison_file_suffix: str = "-gene_summary.tsv"
 
 
 @dataclass
-class VariantBenchmarkPrioritisationOutputGenerator(BenchmarkPrioritisationOutputGenerator):
-    """Subclass of BenchmarkPrioritisationOutputGenerator specialised
+class VariantBenchmarkRunOutputGenerator(BenchmarkRunOutputGenerator):
+    """Subclass of BenchmarkRunOutputGenerator specialised
     for producing variant prioritisation benchmarking outputs."""
 
     prioritisation_type_file_prefix: str = VARIANT_PLOT_FILE_PREFIX
     y_label: str = VARIANT_PLOT_Y_LABEL
-    return_function: Callable[
-        [TrackRunPrioritisation], AnalysisResults
-    ] = TrackRunPrioritisation.return_variant
+    benchmark_function: Callable[
+        [TrackInputOutputDirectories, str, float, defaultdict, RankStatsWriter], BenchmarkRun
+    ] = benchmark_variant_prioritisation
+    rank_comparison_file_suffix: str = "-variant_summary.tsv"
 
 
 @dataclass
-class DiseaseBenchmarkPrioritisationOutputGenerator(BenchmarkPrioritisationOutputGenerator):
-    """Subclass of BenchmarkPrioritisationOutputGenerator specialised
+class DiseaseBenchmarkRunOutputGenerator(BenchmarkRunOutputGenerator):
+    """Subclass of BenchmarkRunOutputGenerator specialised
     for producing disease prioritisation benchmarking outputs."""
 
     prioritisation_type_file_prefix: str = DISEASE_PLOT_FILE_PREFIX
     y_label: str = DISEASE_PLOT_Y_LABEL
-    return_function: Callable[
-        [TrackRunPrioritisation], AnalysisResults
-    ] = TrackRunPrioritisation.return_disease
+    benchmark_function: Callable[
+        [TrackInputOutputDirectories, str, float, defaultdict, RankStatsWriter], BenchmarkRun
+    ] = benchmark_disease_prioritisation
+    rank_comparison_file_suffix: str = "-disease_summary.tsv"
