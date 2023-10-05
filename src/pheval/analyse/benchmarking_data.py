dd9d286b4a40e24c8189f888329d9bfcf7783c07
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-10-05 14:49:17 +0100

    remove TrackRunPrioritisation class and refactoring AnalysisResults to BenchmarkRun

--- src/pheval/analyse/benchmarking_data.py
+++ src/pheval/analyse/benchmarking_data.py
@@ -5,30 +5,9 @@ from pheval.analyse.rank_stats import RankStats
 
 
 @dataclass
-class AnalysisResults:
+class BenchmarkRun:
     """Analysis results for a run."""
 
     results_dir: Path
     ranks: dict
     rank_stats: RankStats
-
-
-@dataclass
-class TrackRunPrioritisation:
-    """Track prioritisation for a run."""
-
-    gene_prioritisation: AnalysisResults = None
-    variant_prioritisation: AnalysisResults = None
-    disease_prioritisation: AnalysisResults = None
-
-    def return_gene(self) -> AnalysisResults:
-        """Return gene prioritisation analysis results."""
-        return self.gene_prioritisation
-
-    def return_variant(self) -> AnalysisResults:
-        """Return variant prioritisation analysis results."""
-        return self.variant_prioritisation
-
-    def return_disease(self) -> AnalysisResults:
-        """Return disease prioritisation analysis results."""
-        return self.disease_prioritisation
