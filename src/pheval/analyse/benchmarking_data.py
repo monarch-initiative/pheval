e9d1aa4cd6d9be39fd274605da02035451933a76
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-12-11 13:52:43 +0000

    refactoring docstrings to follow google docstring format

--- src/pheval/analyse/benchmarking_data.py
+++ src/pheval/analyse/benchmarking_data.py
@@ -6,7 +6,15 @@ from pheval.analyse.rank_stats import RankStats
 
 @dataclass
 class BenchmarkRunResults:
-    """Benchmarking results for a run."""
+    """
+    Benchmarking results for a run.
+
+    Attributes:
+        ranks (dict): Dictionary containing recorded ranks for samples.
+        rank_stats (RankStats): Statistics related to benchmark.
+        results_dir (Path, optional): Path to the result directory. Defaults to None.
+        benchmark_name (str, optional): Name of the benchmark run. Defaults to None.
+    """
 
     ranks: dict
     rank_stats: RankStats
