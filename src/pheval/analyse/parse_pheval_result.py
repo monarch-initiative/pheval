4608c6590f614bd2188edf9c47eb97490f3ee47b
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-05-14 14:52:00 +0100

    Add handling for missing PhEval processed output

--- src/pheval/analyse/parse_pheval_result.py
+++ src/pheval/analyse/parse_pheval_result.py
@@ -1,9 +1,12 @@
+import logging
 from pathlib import Path
 from typing import List
 
 import pandas as pd
 
 from pheval.post_processing.post_processing import PhEvalResult
+
+info_log = logging.getLogger("info")
 
 
 def read_standardised_result(standardised_result_path: Path) -> List[dict]:
@@ -16,7 +19,11 @@ def read_standardised_result(standardised_result_path: Path) -> List[dict]:
     Returns:
         List[dict]: A list of dictionaries representing the content of the standardised result file.
     """
-    return pd.read_csv(standardised_result_path, delimiter="\t").to_dict("records")
+    if standardised_result_path.is_file():
+        return pd.read_csv(standardised_result_path, delimiter="\t").to_dict("records")
+    else:
+        info_log.info(f"Could not find {standardised_result_path}")
+        return pd.DataFrame().to_dict("records")
 
 
 def parse_pheval_result(
