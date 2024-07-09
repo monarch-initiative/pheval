74b29a3fedaba68b55021d447bb304ce772c2164
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-07-09 19:32:58 +0100

    refactor DBConnector

--- src/pheval/analyse/get_connection.py
+++ src/pheval/analyse/get_connection.py
@@ -9,9 +9,23 @@ def dict_factory(cursor, row):
     return d
 
 
-def get_connection() -> DuckDBPyConnection:
-    """
-    Get a connection to the benchmarking results database.
-    """
-    conn = duckdb.connect("analysis.db")
-    return conn
+class DBConnector:
+
+    def __init__(self):
+        self.conn = self.get_connection()
+
+    @staticmethod
+    def get_connection() -> DuckDBPyConnection:
+        conn = duckdb.connect("analysis.db")
+        return conn
+
+    def add_column(self, table_name: str, column: str, default: int=0) -> None:
+        try:
+            self.conn.execute(f'ALTER TABLE {table_name} ADD COLUMN "{column}" INTEGER DEFAULT {default}')
+            self.conn.execute(f'UPDATE {table_name} SET "{column}" = {default}')
+            self.conn.commit()
+        except  duckdb.CatalogException:
+            pass
+
+    def close(self):
+        self.conn.close()
