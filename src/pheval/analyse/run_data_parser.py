edf6faaa0b981d931cbe0336558577725ef32274
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-08-22 15:29:37 +0100

    add classes for parsing benchmarking yaml file for plot customisation

--- src/pheval/analyse/run_data_parser.py
+++ src/pheval/analyse/run_data_parser.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 import yaml
 from pydantic import BaseModel
@@ -26,14 +26,44 @@ class RunConfig(BaseModel):
     disease_analysis: bool
 
 
+class SinglePlotCustomisation(BaseModel):
+    """
+    Store customisations for plots.
+
+    Attributes:
+        plot_type (str): The plot type.
+        rank_plot_title (str): The title for the rank summary plot.
+        roc_curve_title (str): The title for the roc curve plot.
+        precision_recall_title (str): The title for the precision-recall plot.
+    """
+    plot_type: Optional[str]
+    rank_plot_title: Optional[str]
+    roc_curve_title: Optional[str]
+    precision_recall_title: Optional[str]
+
+
+class PlotCustomisation(BaseModel):
+    """
+    Store customisations for all plots.
+    Attributes:
+        gene_plots (SinglePlotCustomisation): Customisation for all gene benchmarking plots.
+        disease_plots (SinglePlotCustomisation): Customisation for all disease benchmarking plots.
+        variant_plots (SinglePlotCustomisation): Customisation for all variant benchmarking plots.
+    """
+    gene_plots: SinglePlotCustomisation
+    disease_plots: SinglePlotCustomisation
+    variant_plots: SinglePlotCustomisation
+
+
 class Config(BaseModel):
     """
     Store configurations for a runs.
     Attributes:
         runs (List[RunConfig]): The list of run configurations.
     """
-
+    benchmark_name: str
     runs: List[RunConfig]
+    plot_customisation: PlotCustomisation
 
 
 def parse_run_config(run_data_path: Path) -> Config:
