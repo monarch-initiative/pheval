9c5ee68ae5a4b9068b92ee4a5a2cb8b2478c0575
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-04-07 18:30:07 +0100

    move logging message to prevent duplicates

--- src/pheval/post_processing/post_processing.py
+++ src/pheval/post_processing/post_processing.py
@@ -151,6 +151,10 @@ def create_empty_pheval_result(
     """
     if result_type in executed_results:
         return
+    logger.info(
+        f"Writing classified results for {len(all_files(phenopacket_dir))} "
+        f"phenopackets to {output_dir.joinpath(f'pheval_{result_type.value}_results')}"
+    )
     executed_results.add(result_type)
     phenopacket_truth_set = PhenopacketTruthSet(phenopacket_dir)
     classify_method, write_method = _get_result_type(result_type, phenopacket_truth_set)
@@ -180,10 +184,6 @@ def generate_gene_result(
         phenopacket_dir (Path): Path to the Phenopacket directory
     """
     output_file = output_dir.joinpath(f"pheval_gene_results/{result_path.stem}-gene_result.parquet")
-    logger.info(
-        f"Writing classified results for {len(all_files(phenopacket_dir))} "
-        f"phenopackets to {output_dir.joinpath('pheval_gene_results')}"
-    )
     create_empty_pheval_result(
         phenopacket_dir, output_dir.joinpath("pheval_gene_results"), ResultType.GENE
     )
@@ -213,10 +213,6 @@ def generate_variant_result(
     """
     output_file = output_dir.joinpath(
         f"pheval_variant_results/{result_path.stem}-variant_result.parquet"
-    )
-    logger.info(
-        f"Writing classified results for {len(all_files(phenopacket_dir))} "
-        f"phenopackets to {output_dir.joinpath('pheval_variant_results')}"
     )
     create_empty_pheval_result(
         phenopacket_dir, output_dir.joinpath("pheval_variant_results"), ResultType.VARIANT
@@ -249,10 +245,6 @@ def generate_disease_result(
     """
     output_file = output_dir.joinpath(
         f"pheval_disease_results/{result_path.stem}-disease_result.parquet"
-    )
-    logger.info(
-        f"Writing classified results for {len(all_files(phenopacket_dir))} "
-        f"phenopackets to {output_dir.joinpath('pheval_disease_results')}"
     )
     create_empty_pheval_result(
         phenopacket_dir, output_dir.joinpath("pheval_disease_results"), ResultType.DISEASE
